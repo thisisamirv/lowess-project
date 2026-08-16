849c7b8420269bcd913dd43227a48832b20c66a3
Amir Valizadeh <amir.valizadeh@yale.edu>
2026-08-16 15:08:35 -0400

    refactor: inline format_julia into Makefile

--- dev/format_julia.jl
+++
@@ -1,6 +0,0 @@
-using Pkg
-Pkg.activate(temp = true)
-Pkg.add("JuliaFormatter")
-using JuliaFormatter
-result = format(["bindings/julia/julia", "tests/julia", "examples/julia"], verbose = true, overwrite = true)
-println("Format result: ", result)
